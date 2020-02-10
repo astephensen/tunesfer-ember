e182d70ccca5bb35f476b1470a44b910f4a05b10
Alan Stephensen <me@alan.io>
2020-02-10 20:57:18 +1100

    Add alpha tag

--- app/initializers/musickit.js
+++ app/initializers/musickit.js
@@ -3,7 +3,7 @@ export function initialize() {
     developerToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IkIyNE5TNzdYQjcifQ.eyJpc3MiOiJVRjVUSzM2VVg1IiwiaWF0IjoxNTgxMTM5NTg2LCJleHAiOjE1OTY2OTE1ODZ9.WW3QjG-xEPo5ceBtkUDAV38JqIA6LFJ7u01PUy85qDQLw5DgLZ2iYm-PnIpOemJLxtS65XeEpGdFNXA81vNWsA',
     app: {
       name: 'Musify',
-      build: '0.1',
+      build: '1.0',
       icon: 'https://musify.alan.io/logo.png'
     }
   });
