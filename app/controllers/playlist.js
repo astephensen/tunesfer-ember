f06ffedac741fc6b21418ccf5b7ac21decb5a9fc
Alan Stephensen <me@alan.io>
2020-02-13 21:51:13 +1100

    Improve concurrency

--- app/controllers/playlist.js
+++ app/controllers/playlist.js
@@ -1,49 +1,34 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-import { task } from 'ember-concurrency';
+import { task, all } from 'ember-concurrency';
 import { TrackItemState } from '../models/track-item';
 
 export default class PlaylistController extends Controller {
   @service musify;
 
-  @tracked
-  processing = false;
-
-  @action
-  async addPlaylist() {
-    const result = await this.musify.authorize();
+  @(task(function *() {
+    const result = yield this.musify.authorize();
     if (!result) {
       return;
     }
 
-    // Update UI.
-    this.processing = true;
-
     // Check if the playlist exists in the library.
-    let playlist = await this.musify.findPlaylist(this.model.playlist.name);
+    let playlist = yield this.musify.findPlaylist(this.model.playlist.name);
     if (!playlist) {
-      playlist = await this.musify.createPlaylist(this.model.playlist.name);
+      playlist = yield this.musify.createPlaylist(this.model.playlist.name);
     }
 
     // Create tasks for each of the tracks.
-    for (const trackItem of this.model.playlist.tracks.items) {
+    let tasks = this.model.playlist.tracks.items.map((trackItem => {
       trackItem.state = TrackItemState.IDLE;
       trackItem.task = this.processTrack.perform(trackItem, playlist);
-    }
-  }
+      return trackItem.task;
+    }));
 
-  @action
-  cancel() {
-    this.processTrack.cancelAll();
-    for (const trackItem of this.model.playlist.tracks.items) {
-      if (trackItem.state === TrackItemState.IDLE || trackItem.state === TrackItemState.PROCESSING) {
-        trackItem.state = TrackItemState.CANCELLED;
-      }
-    }
-    this.processing = false;
-  }
+    // Wait for it all to complete.
+    yield all(tasks);
+  })).restartable() addPlaylist;
 
   @(task(function * (trackItem, playlist) {
     trackItem.state = TrackItemState.PROCESSING;
@@ -69,4 +54,17 @@ export default class PlaylistController extends Controller {
       trackItem.state = TrackItemState.FAILED;
     }
   })).enqueue() processTrack;
+
+  @action
+  cancel() {
+    this.addPlaylist.cancelAll();
+    this.processTrack.cancelAll();
+
+    // Reset track state.
+    for (const trackItem of this.model.playlist.tracks.items) {
+      if (trackItem.state === TrackItemState.IDLE || trackItem.state === TrackItemState.PROCESSING) {
+        trackItem.state = TrackItemState.CANCELLED;
+      }
+    }
+  }
 }
