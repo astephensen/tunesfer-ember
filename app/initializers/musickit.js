3fe334415fe68fef32f0f2cc3a9fa7fa7af8e9fc
Alan Stephensen <me@alan.io>
2020-02-13 18:36:18 +1100

    Rename part 1

--- app/initializers/musickit.js
+++ app/initializers/musickit.js
@@ -2,9 +2,9 @@ export function initialize() {
   MusicKit.configure({
     developerToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IkIyNE5TNzdYQjcifQ.eyJpc3MiOiJVRjVUSzM2VVg1IiwiaWF0IjoxNTgxMTM5NTg2LCJleHAiOjE1OTY2OTE1ODZ9.WW3QjG-xEPo5ceBtkUDAV38JqIA6LFJ7u01PUy85qDQLw5DgLZ2iYm-PnIpOemJLxtS65XeEpGdFNXA81vNWsA',
     app: {
-      name: 'Musify',
+      name: 'Tunesfer',
       build: '1.0',
-      icon: 'https://musify.alan.io/logo.png'
+      icon: 'https://tunesfer.com/logo.png'
     }
   });
 }
