73b1c2eed5c8b44b4dc3898af02144de66dc2e5a
astephensen <astephensen@users.noreply.github.com>
2020-02-27 07:48:07 +0000

    Deploy gh-pages from commit c20e3012c951ae8bd8240b852b7fffb5cd25a4d5

--- assets/tunesfer.js
+++ assets/tunesfer.js
@@ -1682,7 +1682,7 @@ catch(err) {
 
 ;
           if (!runningTests) {
-            require("tunesfer/app")["default"].create({"name":"tunesfer","version":"0.0.0+c4636632"});
+            require("tunesfer/app")["default"].create({"name":"tunesfer","version":"0.0.0+c20e3012"});
           }
         
 //# sourceMappingURL=tunesfer.map
